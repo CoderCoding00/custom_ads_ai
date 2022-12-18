3ea62f62ee3bbf3a61979ba59722c9d500b0d58d
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2022-12-17 21:46:47 -0800

    modified:   client/src/App.js 	modified:   client/src/components/Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 //Import components
-import LoginForm from './components/LoginForm'
-import NavTab from './components/Navbar'
-import Home from './components/Home'
+import LoginForm from "./components/LoginForm";
+import NavTab from "./components/Navbar";
+import Home from "./components/Home";
 // import Display from './components/Display'
 // import Home from './components/Home'
 // import ProductDescription from './components/ProductDescription'
-import SignUp from './components/SignupForm'
-import Product from './components/Product';
-import Audience from './components/Audience';
-import Features from './components/Features';
+import SignUp from "./components/SignupForm";
+import Product from "./components/Product";
+import Audience from "./components/Audience";
+import Features from "./components/Features";
 // import Result from './components/Result';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // ADDED THESE DEC 14
 import {
@@ -22,21 +22,21 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -50,29 +50,28 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-    <div className='App'>
-      <NavTab />
-      {/* <Home /> */}
-      <Routes>
-        {/* <Route path="/" exact element={<//>} /> */}
-        {/* fix later for home */}
-        {/* <Route path="/product-description" element={<ProductDescription/>} /> */}
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignUp />} />
-        {/* <Route path="/display" element={<Display />} /> */}
-        <Route path="/home" element={<Home />} />
-        <Route path="/Product" element={<Product />} />
-        <Route path="/Audience" element={<Audience />} />
-        <Route path="/Features" element={<Features />} />
-        {/* <Route path="/Result" element={<Result />} /> */}
-        {/* <Route path="/Display" element={<Display />} /> */}
-
-      </Routes>
-    </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <NavTab />
+          {/* <Home /> */}
+          <Routes>
+            {/* <Route path="/" exact element={<//>} /> */}
+            {/* fix later for home */}
+            {/* <Route path="/product-description" element={<ProductDescription/>} /> */}
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignUp />} />
+            {/* <Route path="/display" element={<Display />} /> */}
+            <Route path="/home" element={<Home />} />
+            <Route path="/Product" element={<Product />} />
+            <Route path="/Audience" element={<Audience />} />
+            <Route path="/Features" element={<Features />} />
+            {/* <Route path="/Result" element={<Result />} /> */}
+            {/* <Route path="/Display" element={<Display />} /> */}
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
-  
+
 export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,88 +1,85 @@
-import { getToPathname } from '@remix-run/router';
-import React from 'react'
+import { getToPathname } from "@remix-run/router";
+import React from "react";
 // import { useState, useEffect } from 'react';
-import { useState } from 'react';
+import { useState } from "react";
 // import Display from './Display'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button } from "react-bootstrap";
 // import Result from './Result'
-import Audience from './Audience'
+import Audience from "./Audience";
 // import Product from './Product'
 // import Features from './Features'
 
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-    apiKey:'tQPQno7HwoRNVpf0IUv0T3BlbkFJau8C5cDh2lNo3UfR6Ynh',
-      });
-    const openai = new OpenAIApi(configuration);
+  apiKey: "tQPQno7HwoRNVpf0IUv0T3BlbkFJau8C5cDh2lNo3UfR6Ynh",
+});
+const openai = new OpenAIApi(configuration);
 
 const Home = (props) => {
-    const [prompt, setPrompt] = useState({});
-    const [result, setResult] = useState('');
+  const [prompt, setPrompt] = useState({});
+  const [result, setResult] = useState("");
 
+  // const configuration = new Configuration({
+  //     apiKey:'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',
+  //       });
+  //     const openai = new OpenAIApi(configuration);
 
-// const configuration = new Configuration({
-//     apiKey:'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',
-//       });
-//     const openai = new OpenAIApi(configuration);
-    
-    
-    
-    const generatePost = async () => {
-        // let prompt = `Generate a custom social media post for a ${Features} ${Product} that is targeted towards ${Audience}.\n`; 
-        
-        // const res = await openai.createCompletion({
-        //     model:"text-davinci-003",
-        //     prompt: 
-        //     `Generate a custom social media post for a ${props.audienceFormData} ${props.productFormData}  that is targeted towards ${props.featureFormData}.\n`,
-        //     temperature: 0.8,
-        //     max_tokens: 280,
-        //     top_p: 1,
-        //     frequency_penalty: 0,
-        //     presence_penalty: 0,
-        //   });
-        
-        const url = 'https://api.openai.com/v/engines/davinci/completions';
-        const params = {
-            "prompt" : `Generate a custom social media post for a ${props.audienceFormData} ${props.productFormData}  that is targeted towards ${props.featureFormData}.\n`,
-            "max_tokens": 160,
-            "temperature": 0.7,
-            "frequency_penalty": 0.5
-        };
-        const headers = {
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-        };
+  const generatePost = async () => {
+    // let prompt = `Generate a custom social media post for a ${Features} ${Product} that is targeted towards ${Audience}.\n`;
 
-        try{
-            const response = await getToPathname.post(url, {json:params, headers: headers}).json();
-            const output = `${prompt}${response.choice[0].text}`;
-            console.log(output);
-        } catch (err) {
-            console.log(err);
-        }
+    // const res = await openai.createCompletion({
+    //     model:"text-davinci-003",
+    //     prompt:
+    //     `Generate a custom social media post for a ${props.audienceFormData} ${props.productFormData}  that is targeted towards ${props.featureFormData}.\n`,
+    //     temperature: 0.8,
+    //     max_tokens: 280,
+    //     top_p: 1,
+    //     frequency_penalty: 0,
+    //     presence_penalty: 0,
+    //   });
 
-        // setResult(response.data.data[0].url);
- 
+    const url = "https://api.openai.com/v/engines/davinci/completions";
+    const params = {
+      prompt: `Generate a custom social media post for a ${props.audienceFormData} ${props.productFormData}  that is targeted towards ${props.featureFormData}.\n`,
+      max_tokens: 160,
+      temperature: 0.7,
+      frequency_penalty: 0.5,
+    };
+    const headers = {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
     };
 
-        return (
-            <div>
-                <Container>
-                    <h1 className='home-title'> Social Media Post Generator</h1>
-                    <p className='sub-title'>Follow the steps below to generate a social media post!</p>
-          <Audience />
-            <div className="result-main">
-                <h3>Result</h3>
-          
-                <Button onClick={generatePost}>Generate Post</Button>
-                <textarea>{result}</textarea>
-              
-            </div>
-
-                </Container>
-            </div>
-        );
+    try {
+      const response = await getToPathname
+        .post(url, { json: params, headers: headers })
+        .json();
+      const output = `${prompt}${response.choice[0].text}`;
+      console.log(output);
+    } catch (err) {
+      console.log(err);
     }
 
+    // setResult(response.data.data[0].url);
+  };
+
+  return (
+    <div>
+      <Container>
+        <h1 className="home-title"> Social Media Post Generator</h1>
+        <p className="sub-title">
+          Follow the steps below to generate a social media post!
+        </p>
+        <Audience />
+        <div className="result-main">
+          <h3>Result</h3>
+
+          <Button onClick={generatePost}>Generate Post</Button>
+          <textarea>{result}</textarea>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default Home;
